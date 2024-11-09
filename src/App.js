d7ad745f62820a7e392fa4a71ab7e3a979d2ffc3
krishna601135 <kakarapathikrishna@gmail.com>
2024-11-09 17:29:00 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import MainContent from './MainContent';
+function App(){
+  return (
+  <div class='container-fulid'>
+      <Header/>
+     <MainContent/>
+     <Footer/>
+  </div>
+  )
+}
+
+export default App;
+
+
---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import './Footer.css'
+export default function Footer(){
+ return  (
+    <div class="footer">
+      (c) MKK Credit Bank 2024
+   </div>
+ )
+}
